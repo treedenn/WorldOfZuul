59281928e71b68944e2060f94a390e4ef9533435
Lasse Traberg-Larsen <lasse@traberg-larsen.dk>
2017-10-09 10:10:23 +0200

    Added comments to Backpack

--- worldofzuul/Backpack.java
+++ worldofzuul/Backpack.java
@@ -5,23 +5,30 @@ package worldofzuul;
 
 import java.util.Scanner;
 
-// Constructor
-// The capacity is set to 6.
 public class Backpack {
-    private Item[] capacity = new Item[6];
     private int maxCapacity = 6;
+    private Item[] capacity = new Item[maxCapacity]; // The capacity is set to 6.
 
+    /**
+     * No-args constructor
+     */
     public Backpack() {
 
     }
 
-    // Methods,
+    /**
+     * Adds an object of the type Item to the Item[] capacity in the backpack.
+     * If the backpack is full, the user is prompted to choose which item in the backpack to replace.
+     * @param item  an object of the type Item
+     * @return  true/false if succeeded/failed
+     * @see Item, addItem(), removeItem()
+     */
     public boolean addItem(Item item) {
         if (getCurrentCapacity() == capacity.length) {
             Scanner input = new Scanner(System.in);
             System.out.println("Oh no, your backpack is full! Choose which thing in your backpack to replace: ");
             for (int i = 0; i < capacity.length; i++) {
-                System.out.println((i + 1) + ": " + Item[i]);
+                System.out.println((i + 1) + ": " + capacity[i]);
             }
 
             int chosenIndex;
@@ -51,17 +58,32 @@ public class Backpack {
         return true;
     }
 
+    /**
+     * Adds an object of the type Item to a specified index
+     * @param item  an object of the type Item
+     * @param index the index of Item[] capacity to which the item should be added, specified as an int.
+     * @return  returns true when item is added to the Item[] capacity.
+     */
     public boolean addItem(Item item, int index) {
         capacity[index] = item;
         return true;
     }
 
-    // Has to be redone, item must be replaced.
+    /**
+     * Empties Item[] capacity at a specified index by overriding with null.
+     * @param index the index of Item[] capacity which should be emptied, specified as an int.
+     * @return  returns true when an index of the Item[] capacity is emptied.
+     */
     public boolean removeItem(int index) {
         capacity[index] = null;
-        return true;
+        return true; // Has to be redone, item must be replaced.
     }
 
+    /**
+     * Overrides a given object of the type Item with null in Item[] capacity.
+     * @param item  an object of the type Item
+     * @return returns true if item is found in Item[] capacity, and the item is removed.
+     */
     public boolean removeItem(Item item) {
         for (int i = 0; i < capacity.length; i++) {
             if (capacity[i] == item) {
@@ -72,14 +94,10 @@ public class Backpack {
         return false;
     }
 
-    public int getMaxCapacity() {
-        return maxCapacity;
-    }
-
-    public void setMaxCapacity(int maxCapacity) {
-        this.maxCapacity = maxCapacity;
-    }
-
+    /**
+     * Calculates the current capacity of the backpack
+     * @return  the remaining space in form of an int
+     */
     public int getCurrentCapacity() {
         int count = 0;
         for (Item item : capacity) {
@@ -88,6 +106,22 @@ public class Backpack {
             }
         }
         return maxCapacity - count;
+    }
+
+    /**
+     * Gets the maxCapacity variable
+     * @return  max capacity in form of an int
+     */
+    public int getMaxCapacity() {
+        return maxCapacity;
+    }
+
+    /**
+     * Sets the maxCapacity variable
+     * @param integer
+     */
+    public void setMaxCapacity(int maxCapacity) {
+        this.maxCapacity = maxCapacity;
     }
 
 }
