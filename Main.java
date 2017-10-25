63a48474a5c2afca061c30b1fd3a99ba854ac440
AsianKiddo <lalala-josef@hotmail.com>
2017-10-25 13:05:57 +0200

    Fixed tankstation/quiz

--- Main.java
+++ Main.java
@@ -3,38 +3,18 @@ import BLL.Game;
 import DAL.item.Color;
 import DAL.item.Item;
 import DAL.item.ItemType;
-<<<<<<< HEAD
-=======
+
 import DAL.item.State;
 import DAL.yaml.ItemParser;
 
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Map;
->>>>>>> dc2e479b572423a433e77c62987b1174126f552a
 
 public class Main {
 	/* runs the game */
 	public static void main(String[] args) {
 		new Game().start();
-<<<<<<< HEAD
-=======
-
-		/* Testing the item parser to obtain the item database from text
-		ItemParser parser = ItemParser.getInstance();
-
-		try {
-			Map<Integer, Map<String, Object>> map = parser.getDatabase();
-			System.out.println(map.get(0).get("name"));
-		} catch(IOException ex) {
-			ex.printStackTrace();
-		}
-		*/
-
-
-
-
->>>>>>> dc2e479b572423a433e77c62987b1174126f552a
 	}
 
 	private static void itemTemplate(int id, String name, ItemType type, Color color, State state) {
