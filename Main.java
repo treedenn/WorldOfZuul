ac89e9ab96535dd29370010758ac4966706a3f8e
Dennis Petersen <Dennis-drengen@hotmail.dk>
2017-11-07 21:26:13 +0100

    Implements start menu

--- Main.java
+++ Main.java
@@ -1,14 +1,30 @@
 
 import BLL.Game;
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.layout.AnchorPane;
+import javafx.stage.Stage;
 
-public class Main {
+public class Main extends Application {
 	/*
 		Phase 1 is complete!
 			Date: 03/11/2017
 	 */
 
+	@Override
+	public void start(Stage primaryStage) throws Exception {
+		AnchorPane pane = FXMLLoader.load(getClass().getResource("UI/view/start_view.fxml"));
+
+		primaryStage.setScene(new Scene(pane, pane.getPrefWidth(), pane.getPrefHeight()));
+		primaryStage.setTitle("Rick's Adventure - Group 24");
+		primaryStage.setResizable(false);
+
+		primaryStage.show();
+	}
+
 	/* runs the game */
 	public static void main(String[] args) {
-		new Game().start();
+		Application.launch(args);
 	}
 }
--- UI/controller/StartController.java
+++ UI/controller/StartController.java
@@ -1,4 +1,28 @@
 package UI.controller;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+
 public class StartController {
+
+	@FXML private Button buttonNewGame;
+	@FXML private Button buttonExit;
+	@FXML private Button buttonHighscore;
+
+	@FXML
+	void handleExitAction(ActionEvent event) {
+
+	}
+
+	@FXML
+	void handleHighscoreAction(ActionEvent event) {
+
+	}
+
+	@FXML
+	void handleNewGameAction(ActionEvent event) {
+
+	}
+
 }
