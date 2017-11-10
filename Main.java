e960fd5eaede03c995d7db4fcc97c75169b6e83c
Dennis Petersen <Dennis-drengen@hotmail.dk>
2017-11-10 12:01:39 +0100

    Clean imports and added listview for highscore

--- Main.java
+++ Main.java
@@ -1,7 +1,3 @@
-
-import BLL.Game;
-import DAL.scoring.HighscoreManager;
-
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
@@ -9,17 +5,12 @@ import javafx.scene.layout.AnchorPane;
 import javafx.stage.Stage;
 
 public class Main extends Application {
-	/*
-		Phase 1 is complete!
-			Date: 03/11/2017
-	 */
-
 	@Override
 	public void start(Stage primaryStage) throws Exception {
 		AnchorPane pane = FXMLLoader.load(getClass().getResource("UI/view/start_view.fxml"));
 
 		primaryStage.setScene(new Scene(pane, pane.getPrefWidth(), pane.getPrefHeight()));
-		primaryStage.setTitle("Rick's Adventure - Group 24");
+		primaryStage.setTitle("Rick's Adventure");
 		primaryStage.setResizable(false);
 
 		primaryStage.show();
@@ -27,12 +18,7 @@ public class Main extends Application {
 
 	/* runs the game */
 	public static void main(String[] args) {
-		new Game().start();
-
-
-
 		Application.launch(args);
-
 	}
 }
 
--- UI/controller/StartController.java
+++ UI/controller/StartController.java
@@ -1,14 +1,16 @@
 package UI.controller;
 
+import DAL.scoring.Score;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
+import javafx.scene.control.ListView;
 
 public class StartController {
-
 	@FXML private Button buttonNewGame;
 	@FXML private Button buttonExit;
 	@FXML private Button buttonHighscore;
+	@FXML private ListView<Score> listviewHighscore;
 
 	@FXML
 	void handleExitAction(ActionEvent event) {
