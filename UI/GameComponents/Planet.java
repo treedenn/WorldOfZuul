0362713e6061e75061e5a53e5c08e4f47829fb31
Lasse Traberg-Larsen <lasse@traberg-larsen.dk>
2017-11-25 17:36:25 +0100

    A little bit of performance optimizing...

--- UI/GameComponents/Planet.java
+++ UI/GameComponents/Planet.java
@@ -24,7 +24,6 @@ public class Planet extends GameObject {
 
     private static List<Planet> planets = new ArrayList<>();
     private StackPane planetWrapper;
-    private Circle atmosphere;
     private Label name;
     private Sphere sphere;
     PhongMaterial texture;
@@ -42,17 +41,11 @@ public class Planet extends GameObject {
         sphere.setMaterial(texture);
 
 
-        atmosphere = new Circle(sphere.getRadius()*4);
-        Stop[] atmosphereStops = {new Stop(0,Color.rgb(60,159,154, 0.5)), new Stop(sphere.getRadius()/atmosphere.getRadius() + 0.05, Color.rgb(15,55,67,0.8)), new Stop(1, Color.rgb(15,55,67,0))};
-        RadialGradient rg1 = new RadialGradient(0,0,atmosphere.getCenterX(), atmosphere.getCenterY(), atmosphere.getRadius(), false, CycleMethod.NO_CYCLE, atmosphereStops);
-        atmosphere.setFill(rg1);
-        atmosphere.setCache(true);
-
-        Group planetGroup = new Group(atmosphere, sphere, name);
+        Group planetGroup = new Group(sphere, name);
         planetWrapper.getChildren().add(planetGroup);
 
-        planetGroup.setTranslateX(-atmosphere.getRadius());
-        planetGroup.setTranslateY(-atmosphere.getRadius());
+        planetGroup.setTranslateX(-sphere.getRadius());
+        planetGroup.setTranslateY(-sphere.getRadius());
 
 
 
