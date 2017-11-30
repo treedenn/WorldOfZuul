05427a79776665a6eaec900d8507f81f765a3dd8
Michael poulsen <fartillucas@gmail.com>
2017-11-30 12:50:46 +0100

    JavaDoc for Iquiz

--- BLL/ACQ/IQuiz.java
+++ BLL/ACQ/IQuiz.java
@@ -1,5 +1,11 @@
 package BLL.ACQ;
 
+/**
+ * gets question for quiz
+ * @returns question
+ * gets possiple answers
+ * @returns possible answers
+ */
 public interface IQuiz {
     String getQuestion();
     String[] getOptions();
