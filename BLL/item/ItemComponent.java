6174dfdbb6a1b7ae25bc24a20ca8357cc267725c
Dennis Petersen <Dennis-drengen@hotmail.dk>
2017-11-30 23:15:53 +0100

    Javadoc on Item and derived classes

--- BLL/item/ItemComponent.java
+++ BLL/item/ItemComponent.java
@@ -1,5 +1,8 @@
 package BLL.item;
 
+/**
+ * This defines the functionality of Components within the game.
+ */
 public class ItemComponent extends Item {
 	private ComponentType componentType;
 	private Color color;
@@ -12,14 +15,28 @@ public class ItemComponent extends Item {
 		this.state = state;
 	}
 
+	/**
+	 * Gets the type of the component, declared by the enum {@link ComponentType}.
+	 * @return
+	 */
 	public ComponentType getComponentType() {
 		return componentType;
 	}
 
+	/**
+	 * Gets the color of the component.
+	 * {@link Color} is homemade.
+	 * @return
+	 */
 	public Color getColor() {
 		return color;
 	}
 
+	/**
+	 * Gets the state of the component, declared by the enum {@link State}.
+	 * State is abstract, however, it can be compared to a condition.
+	 * @return the state
+	 */
 	public State getState() {
 		return state;
 	}
