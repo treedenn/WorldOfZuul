90bfa531b19ed408f31c98fac22608310423a8e2
Dennis Petersen <Dennis-drengen@hotmail.dk>
2017-11-30 12:18:27 +0100

    Javadoc to IScore

--- BLL/ACQ/IScore.java
+++ BLL/ACQ/IScore.java
@@ -1,6 +1,15 @@
 package BLL.ACQ;
 
 public interface IScore {
+	/**
+	 * Gets the player name.
+	 * @return player name
+	 */
 	String getName();
+
+	/**
+	 * Gets the score of the player.
+	 * @return the score
+	 */
 	int getScore();
 }
