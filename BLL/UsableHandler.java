10a1261989334f30df0e7beaf7ceda3dea5c02c1
Dennis Petersen <Dennis-drengen@hotmail.dk>
2017-12-01 02:17:29 +0100

    Javadoc on BLL handlers and a javadoc change to inventory.

--- BLL/UsableHandler.java
+++ BLL/UsableHandler.java
@@ -4,12 +4,18 @@ import BLL.ACQ.Usable;
 import BLL.item.usable.UsablePortalGun;
 import BLL.item.usable.UsableTransformationElixir;
 
+/**
+ * Handles all the {@link Usable} objects, by returning a usable depending on the index giving.
+ * It is only used in the data layer, when creating new items.
+ * If they have a usable id, the id will be sent here and will return the corresponding usable effect.
+ */
 public class UsableHandler {
+	/**
+	 * Gets the usable at given index.
+	 * @param index index of usable
+	 * @return usable at given index
+	 */
 	public Usable getUsable(int index) {
-		return obtainUsable(index);
-	}
-
-	private Usable obtainUsable(int index) {
 		switch(index) {
 			case 0: return new UsablePortalGun();
 			case 1: return new UsableTransformationElixir();
