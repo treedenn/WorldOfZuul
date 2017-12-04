5fa04c65f27006b8c5c065d800d520262c4cacee
Dennis Petersen <Dennis-drengen@hotmail.dk>
2017-12-04 11:44:52 +0100

    JavaDoc on Recipe

--- BLL/character/player/Recipe.java
+++ BLL/character/player/Recipe.java
@@ -3,6 +3,9 @@ package BLL.character.player;
 import BLL.ACQ.IItemStack;
 import BLL.item.Item;
 
+/**
+ * Recipe is the object holding all the requirements for the player to obtain.
+ */
 public class Recipe {
 	private Item[] requirements;
 
@@ -10,10 +13,20 @@ public class Recipe {
 		this.requirements = requirements;
 	}
 
+	/**
+	 * Gets an array of the requirements as {@link Item}.
+	 * @return array of requirements
+	 */
 	public Item[] getRequirements() {
 		return requirements;
 	}
 
+	/**
+	 * Searches the recipe for the given items.
+	 * A boolean will be set based on existing.
+	 * @param itemStacks the itemstacks to search for
+	 * @return a boolean array of whether the items is in the recipe
+	 */
 	public boolean[] haveItems(IItemStack[] itemStacks) {
 		boolean[] containItems = new boolean[4];
 
@@ -28,6 +41,11 @@ public class Recipe {
 		return containItems;
 	}
 
+	/**
+	 * Checks if the item exists in the recipe
+	 * @param item the item to search for
+	 * @return true, if exists
+	 */
 	public boolean hasItem(Item item) {
 		for(Item requirement : requirements) {
 			if(requirement.getName().equals(item.getName())) {
